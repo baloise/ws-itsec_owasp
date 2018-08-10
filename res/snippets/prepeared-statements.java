ea06fb34681b24e582407617ff76cd5720cb6ecc
MrCode97 <robin.seidel@epost.ch>
2018-08-10 14:47:56 +0200

    shorten code

--- res/snippets/prepeared-statements.java
+++ res/snippets/prepeared-statements.java
@@ -1,6 +1,6 @@
 String custname = request.getParameter("customerName");
 
-String query = "SELECT account_balance FROM user_data WHERE user_name = ? ";
+String query = "SELECT acc_bal FROM user_data WHERE user_name = ? ";
 PreparedStatement pstmt = connection.prepareStatement( query );
 
 pstmt.setString( 1, custname);
